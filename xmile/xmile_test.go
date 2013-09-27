90159eae6b27ff76c7e3adf397ce17f32c208c8f
Bobby Powers <bobbypowers@gmail.com>
2013-09-26 23:51:36 -0400

    start work on writing graphviz dot file

--- xmile/xmile_test.go
+++ xmile/xmile_test.go
@@ -5,13 +5,79 @@
 package xmile_test
 
 import (
-	"encoding/xml"
+	"bufio"
+	"bytes"
+	"fmt"
 	xmile "github.com/bpowers/go-xmile/compat"
 	"io/ioutil"
 	"os"
 	"testing"
+	"text/template"
 )
 
+const dotTmpl = `
+digraph model {
+{{range $.Clouds}}
+{{.}} [shape=none,label=""]{{end}}
+
+{{range $.Stocks}}
+{{.Name}} [shape=box]{{end}}
+
+{{range $.Flows}}
+{{.Name}} [shape=circle]{{end}}
+
+{{range $.Auxs}}
+{{.Name}} [shape=circle]{{end}}
+
+{{range $.Stocks}}{{with $s := .}}{{range $.OutsC}}
+{{$s.Name}} -> {{.}}{{end}}{{end}}{{end}}
+
+{{range $.Auxs}}{{with $s := .}}{{range $.OutsC}}
+{{$s.Name}} -> {{.}}{{end}}{{end}}{{end}}
+}
+`
+
+type DotData struct {
+	Clouds []string
+	Stocks []VInfo
+	Flows  []VInfo
+	Auxs   []VInfo
+}
+
+// VInfo stores information about a var for use by dot.
+//
+// stock: only connectors out.
+// flow: all 3, connector outs & flow ins and outs.
+// aux: only outsc.
+type VInfo struct {
+	Name  string
+	OutsC []string
+	OutsF []string
+	Ins   []string // only flows have ins
+}
+
+func writeDot(f *xmile.File) error {
+	w := bufio.NewWriter(os.Stderr)
+	defer w.Flush()
+
+	var data DotData
+
+	var buf bytes.Buffer
+	tmpl := template.New("model.dot")
+	if _, err := tmpl.Parse(dotTmpl); err != nil {
+		return fmt.Errorf("Parse(dotTmpl): %s", err)
+	}
+	if err := tmpl.Execute(&buf, &data); err != nil {
+		return fmt.Errorf("Execute: %s", err)
+	}
+
+	w.Write(buf.Bytes())
+	w.Write([]byte("\n"))
+
+	return nil
+}
+
+/*
 func TestRead(t *testing.T) {
 	contents, err := ioutil.ReadFile("../models/pred_prey.stmx")
 	if err != nil {
@@ -31,4 +97,22 @@ func TestRead(t *testing.T) {
 	os.Stderr.Write([]byte(xmile.XMLDeclaration + "\n"))
 	os.Stderr.Write(output)
 	os.Stderr.Write([]byte("\n"))
+}
+*/
+
+func TestDot(t *testing.T) {
+	contents, err := ioutil.ReadFile("../models/pred_prey.stmx")
+	if err != nil {
+		t.Fatalf("ioutil.ReadFile: %s", err)
+	}
+
+	f, err := xmile.ReadFile(contents)
+	if err != nil {
+		t.Fatalf("xmile.ReadFile: %s", err)
+	}
+
+	f.Models[0].Interface = xmile.View{}
+	if err := writeDot(f); err != nil {
+		t.Fatalf("writeDot: %s", err)
+	}
 }
